09db557dddeb60622c1dbe70da74527a26bce810
wclee <msc0953@gmail.com>
2019-10-12 01:38:10 +0800

    feat(test): add some files for test check robot file name hook

--- code_review_hooks/check_robotframework_files.py
+++ code_review_hooks/check_robotframework_files.py
@@ -1,5 +1,67 @@
+import os
+
+from robot.model import SuiteVisitor
+
+
+class BaseChecker(SuiteVisitor):
+    id = 0
+    severity = 'I'
+    name = 'base'
+
+    def __init__(self, findings):
+        super().__init__()
+        self.findings = findings
+
+    def add_finding(self, category, node, msg):
+        self.findings.add((category, node, msg))
+
+
+class NamingChecker(BaseChecker):
+    def start_suite(self, suite):
+        if '.' in os.path.basename(suite.source):
+            print('suite name should not contain "."')
+
+    def start_test(self, test):
+        if '.' in test.name:
+            print('test name should not contain ".')
+
+    def check_suite_variable_name(self, variable):
+        pass
+
+
+class StructureChecker(BaseChecker):
+    def start_suite(self, suite):
+        pass
+
+    def start_test(self, test):
+        pass
+
+    def check_setup_teardown_in_suite(self, suite):
+        pass
+
+
+class TagsCheck(BaseChecker):
+    def start_test(self, test):
+        pass
+
+    def get_tags(self, test):
+        pass
+
+
+class KeywordInvalidUsageChecker(BaseChecker):
+    def start_keyword(self, keyword):
+        pass
+
+
+class CaseDuplicateChecker(BaseChecker):
+    def start_test(self, test):
+        pass
+
+
 def main():
-    print("check robotframework files")
+    raise SystemExit(
+        'check-robotframework is un-implemented.'
+    )
 
 
 if __name__ == '__main__':
---
+++ tests/testdata/badname.robot
@@ -0,0 +1,10 @@
+*** Settings ***
+Suite Setup  Begin Suite
+Suite Teardown  End Suite
+
+*** Keywords ***
+Begin Suite
+    Log To Console  "Begin Suite"
+
+End Suite
+    Log To Console  "End Suite"
